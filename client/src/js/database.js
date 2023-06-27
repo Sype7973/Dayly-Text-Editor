0e9b0974740bf819f92069795165223cfb7ca2e1
unknown <adam.day7973@gmail.com>
2023-06-27 20:20:11 +1000

    updated database get

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,10 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.log('putDb');
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   await store.put(content);
+  console.log('putDb put', content);
   await tx.done;
   console.log('putDb done');
 };
