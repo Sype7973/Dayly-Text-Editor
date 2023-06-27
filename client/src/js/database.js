da7fb6177b4bcc5e5b698dcc217ad9ad5bed2e33
unknown <adam.day7973@gmail.com>
2023-06-27 22:30:09 +1000

    fixed database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,33 +12,33 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.log('putDb');
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  export const putDb = async (content) => {
+    console.log('Updating database...');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    try {
+      const request = store.put({ id: 1,  value: content });
+      const result = await request;
+    console.log('Data saved to the database');
+    } catch (err) {
+      console.log(err);
+    }
+  };
   
-  try {
-    const request = store.put({ id: 1, value: content });
+  // TODO: Add logic for a method that gets all the content from the database
+  export const getDb = async () => {
+    console.log('Getting data from database...');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);
     const result = await request;
-  } catch (error) {
-    console.error('putDb error', error);
-  }
-};
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  console.log('getDb');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
-  const result = await request;
-  console.log(result);
-  console.log('Data retrieved', result);
-
-  return result;
-};
+    if (result) {
+      console.log('Data retrieved', result);
+      return result.value;
+    }
+    return false;
+  };
 
 initdb();
