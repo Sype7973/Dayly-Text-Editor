4ee1261e613a692e8503338c5c2c641d9d549a6b
unknown <adam.day7973@gmail.com>
2023-06-27 18:12:34 +1000

    updated package.json and webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: './index.html',
         title: 'Dayly Text Editor'
       }),
       new InjectManifest({
